4e023ed211195301e54a91d5f802c9f1df725bb3
dvschaitanya <chaitanyadvs143@gmail.com>
2025-03-10 18:41:24 +0530

    First commit for fetcch users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  let [names, setNames] = useState([]);
+  let [defaultNames, setDefaultNames] = useState([]);
+  let [count, setCount] = useState(0);
+  let [search, setSearch] = useState('');
+
+  const fetchUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=> res.json())
+    .then((data) => {
+      setNames(data)
+      setDefaultNames(data);
+    })
+    .catch((err)=> {
+      console.error(err);
+    })
+  } 
+ useEffect (()=>{
+    fetchUsers();
+ }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am dvs</h1>
+      <input
+      type = 'text'
+      value = {search}
+      onChange={(e)=> {
+        setSearch(e.target.value)
+        let filterName = defaultNames.filter((item, index) => item.name.toLowerCase().includes(search.toLowerCase()));
+        setNames(filterName);
+        if (e.target.value === '') {
+          setNames(defaultNames);
+        }
+      }}
+      ></input>
+      <ul>{
+        names.map((name, index) => (
+          <li key = {name.id}> {name.name}</li>
+        ))
+        }</ul>
+    <h1>My count is {names.length}</h1>
+    <button onClick={()=> {
+      setCount(count+1)
+    }}>Increment</button>
     </div>
   );
 }
